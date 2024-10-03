63b199bff3e65d49f6af4027ae08523e5e8ef3b7
Иван Трубников <i.a.trubnikov@gmail.com>
2024-10-03 11:39:44 +0300

    start

---
+++ src/main/java/ru/javalevelup/JavaLevelUpApplication.java
@@ -0,0 +1,14 @@
+package ru.javalevelup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLevelUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLevelUpApplication.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/ru/javalevelup/JavaLevelUpApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javalevelup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLevelUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
