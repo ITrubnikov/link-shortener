1bfb29cf06a087f10b0304145358a83a0c45b354
Иван Трубников <i.a.trubnikov@gmail.com>
2024-11-02 23:12:16 +0300

    hw3

--- src/main/java/ru/linkshortener/service/LinkInfoService.java
+++ src/main/java/ru/linkshortener/service/LinkInfoService.java
@@ -2,7 +2,12 @@ package ru.linkshortener.service;
 
 import ru.linkshortener.dto.CreateLinkInfoRequest;
 import ru.linkshortener.dto.LinkInfoResponse;
+import ru.linkshortener.exception.NotFoundExeption;
+
+import java.util.List;
 
 public interface LinkInfoService {
     LinkInfoResponse getByShortLink (String shortLink);
+    LinkInfoResponse createLinkInfo(CreateLinkInfoRequest request);
+    List<LinkInfoResponse> findByFilter();
 }
