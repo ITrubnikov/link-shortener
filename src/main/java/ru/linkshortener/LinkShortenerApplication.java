657defb91f9cddd8109b7f21358b438a60b21b57
Иван Трубников <i.a.trubnikov@gmail.com>
2025-01-26 16:00:35 +0300

    refactor

--- src/main/java/ru/linkshortener/LinkShortenerApplication.java
+++ src/main/java/ru/linkshortener/LinkShortenerApplication.java
@@ -2,7 +2,7 @@ package ru.linkshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import util.LoggingStarterAutoConfiguration;
+import ru.itrubnikov.loggingstarter.LoggingStarterAutoConfiguration;
 
 @SpringBootApplication
 public class LinkShortenerApplication {
